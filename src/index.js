46e940ebce6fd8528fd0af4fa3ec15f592ccae85
markazie <markazie@gmail.com>
2019-07-16 14:15:04 +0100

    add read and write - google spreadsheet

--- src/index.js
+++ src/index.js
@@ -1,20 +1,72 @@
-import bodyParser from 'body-parser';
-import express from 'express';
-import health from './routes/health';
+import bodyParser from "body-parser"
+import express from "express"
+import health from "./routes/health"
+const GoogleSheet = require("google-spreadsheet")
+const { promisify } = require("util")
+const creds = require("./client_access.json")
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  next();
-});
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+})
 
-app.use('/health', health);
-app.get('/', (req, res) => res.send('Hello World!'));
+//Fetch all the data from spreadsheet
+async function fetchSpreadsheet() {
+  const doc = new GoogleSheet("1sZHp76yPeLfuXeigccDwGDk4bU_abVQYeOVzzZ9oiwE")
+  await promisify(doc.useServiceAccountAuth)(creds)
+  const info = await promisify(doc.getInfo)()
+  const sheet = info.worksheets[0]
+  const rows = await promisify(sheet.getRows)({
+    offset: 1
+  })
+  const data = rows.map(row => [
+    row.name,
+    row.classtype,
+    row.expensetype,
+    row.amount,
+    row.date
+  ])
+  return data
+}
 
-app.listen('8080', () => {
-  console.log('Starting application on port: 8080');
-});
+//Add data to spreadsheet
+async function spreadsheet(name, classtype, expensetype, amount, date) {
+  const doc = new GoogleSheet("1sZHp76yPeLfuXeigccDwGDk4bU_abVQYeOVzzZ9oiwE")
+  await promisify(doc.useServiceAccountAuth)(creds)
+  const info = await promisify(doc.getInfo)()
+  const sheet = info.worksheets[0]
+
+  const row = {
+    name,
+    classtype,
+    expensetype,
+    amount,
+    date
+  }
+
+  await promisify(sheet.addRow)(row)
+}
+
+app.use("/health", health)
+app.get("/", (req, res) => res.send("Hello World!"))
+app.get("/expense/list", async (req, res) => {
+  const list = await fetchSpreadsheet()
+  res.status(200).send(list)
+})
+
+app.post("/expense/submit", (req, res) => {
+  const { name, classtype, expensetype, amount, date } = req.body
+  spreadsheet(name, classtype, expensetype, amount, date)
+  res.sendStatus(200)
+})
+
+
+app.listen("8080", () => {
+  console.log("Starting application on port: 8080")
+})
