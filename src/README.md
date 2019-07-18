13b8b74e1de4d00e43eafb672e70ac934eb373a5
markazie <markazie@gmail.com>
2019-07-18 11:31:29 +0100

    Add feature / send confirmation email to user

---
+++ src/README.md
@@ -0,0 +1,19 @@
+# Endpoints
+
+You can fetch data from expenses list or send data to expenses list also, confirmation email will be send to user.
+
+## Fetch expenses endpoint
+
+```
+/expense/list
+```
+
+## Send expenses and send confirmation email endpoint
+
+```
+/expense/submit
+```
+
+Body format: { name, classtype, expensetype, amount, date }
+
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+require("dotenv").config()
 import bodyParser from "body-parser"
 import express from "express"
+import nodemailer from "nodemailer"
 import health from "./routes/health"
 const GoogleSheet = require("google-spreadsheet")
 const { promisify } = require("util")
 const creds = require("./client_access.json")
+const Emails = require("./Emails.json")
 
 const app = express()
 
@@ -53,6 +56,38 @@ async function spreadsheet(name, classtype, expensetype, amount, date) {
   await promisify(sheet.addRow)(row)
 }
 
+//Send an email to person who submit the expense form
+async function mailer(name) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASS
+    }
+  })
+  const html = `<div>
+  <p>Dear ${name}  </p> 
+  <p>Thankyou for <strong>submitting your expense </strong>
+  If you need any further information please contact us  </p>
+  
+  <p>kind Regards </p>
+  <p>Code your future team</p>
+   </div>`
+
+  const emailData = {
+    to: Emails[name],
+    subject: "Thank you for submitting your expense",
+    html,
+    replyToEmail: "Do not reply"
+  }
+  try {
+    const sendEmail = await transporter.sendMail(emailData)
+    return sendEmail
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
 app.use("/health", health)
 app.get("/", (req, res) => res.send("Hello World!"))
 app.get("/expense/list", async (req, res) => {
@@ -62,10 +97,10 @@ app.get("/expense/list", async (req, res) => {
 
 app.post("/expense/submit", (req, res) => {
   const { name, classtype, expensetype, amount, date } = req.body
+  mailer(name)
   spreadsheet(name, classtype, expensetype, amount, date)
   res.sendStatus(200)
 })
-
 
 app.listen("8080", () => {
   console.log("Starting application on port: 8080")
